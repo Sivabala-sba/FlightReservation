cde24036dbf14c5d0c372098d09b43e0c251b496
Sivabala-sba <sivabalasba@gmail.com>
2024-02-02 12:47:35 +0530

    All files are updated and errors are cleared

--- src/main/java/com/FlightReservation/util/EmailUtil.java
+++ src/main/java/com/FlightReservation/util/EmailUtil.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
 
+@Component
 public class EmailUtil {
 
     @Value("${com.FlightReservation.itinerary.email.body}")
